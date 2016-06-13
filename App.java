6e18c0074b65c001276e7483d5f9e5dac21be769
samsunli <samsunli@users.noreply.github.com>
2016-06-13 11:40:59 +0800

    Just
    Test

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.mnl.test1.Test3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
